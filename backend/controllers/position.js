e8a9c4cba10c066b5c24460cf879f5848d3fede9
r.tereshchenko <r.tereschchenko@seclgroup.com>
2020-12-24 13:44:33 +0200

    positions -> updating controller/adding responses to client's requests

--- backend/controllers/position.js
+++ backend/controllers/position.js
@@ -1,23 +1,53 @@
-module.exports.getByPositionId = function (req, res) {
-    res.status(200).json({
-        getByPositionId: true
-    })
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler');
+
+
+module.exports.getPositionsByCategoryId = async function (req, res) {
+    try {
+        const positions = await Position.find({
+            category: req.params.categoryId,
+            user: req.user.id
+        })
+        res.status(200).json(positions)
+    } catch (error) {
+        errorHandler(res, error)
+    }
 }
 
-module.exports.removePosition = function (req, res) {
-    res.status(200).json({
-        removePosition: true
-    })
+module.exports.removePosition = async function (req, res) {
+    try {
+        await Position.remove({_id: req.params.id})
+        res.status(200).json({
+            message: 'Position has been removed'
+        })
+    } catch (error) {
+        errorHandler(res, error)
+    }
 }
 
-module.exports.createPosition = function (req, res) {
-    res.status(200).json({
-        createPosition: true
-    })
+module.exports.createPosition = async function (req, res) {
+    try {
+        const position = await new Position({
+            name: req.body.name,
+            cost: req.body.cost,
+            category: req.body.category,
+            user: req.user.id
+        }).save()
+        res.status(201).json(position)
+    } catch (error) {
+        errorHandler(res, error)
+    }
 }
 
-module.exports.updatePosition = function (req, res) {
-    res.status(200).json({
-        updatePosition: true
-    })
+module.exports.updatePosition = async function (req, res) {
+    try {
+        const updatedPosition = await Position.findOneAndUpdate(
+            {_id: req.params.id},
+            {$set: req.body},
+            {new: true}
+            )
+        req.status(200).json(updatedPosition)
+    } catch (error) {
+        errorHandler(res, error)
+    }
 }
