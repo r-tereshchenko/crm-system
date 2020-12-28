87f55516b06d3409a7adadefc1dea80809db9e10
r.tereshchenko <r.tereschchenko@seclgroup.com>
2020-12-28 19:12:44 +0200

    working on login page; auth service -> login logic; creating interfaces

--- frontend/src/app/login-page/login-page.component.ts
+++ frontend/src/app/login-page/login-page.component.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Subscription } from 'rxjs';
+
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
+  form: FormGroup
 
-  constructor() { }
+  // Subscriptions
+  authLoginSub: Subscription
 
-  ngOnInit(): void {
+  constructor(
+    private authS: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
   }
 
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(4)
+      ])
+    })
+
+    this.route.queryParams.subscribe((params) => {
+      if (params['registered']) {
+      //  Please log in using your registered data
+      } else if (params['accessDenied']) {
+      //  please authorize
+      }
+    })
+  }
+
+  // Getters for form controls
+  public get emailControl(): AbstractControl {
+    return this.form.get('email')
+  }
+
+  public get passwordControl(): AbstractControl {
+    return this.form.get('password')
+  }
+
+  submitForm(): void {
+    this.form.disable();
+
+    this.authLoginSub = this.authS.login(this.form.value).subscribe(
+      response => {
+        this.router.navigate(['/overview'])
+        console.log('Login success: ', response);
+      },
+      error => {
+        console.log('Login error: ', error);
+        this.form.enable()
+      }
+      )
+  }
+
+  ngOnDestroy(): void {
+    if (this.authLoginSub) {
+      this.authLoginSub.unsubscribe()
+      this.authLoginSub = null;
+    }
+  }
 }
---
+++ frontend/src/app/shared/services/auth.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+import { LoginResponseToken, User } from '../interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthService {
+
+  private token: string | null = null;
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  getToken(): string {
+    return this.token
+  }
+
+  setToken(token: string): void {
+    this.token = token
+  }
+
+  login(user: User): Observable<LoginResponseToken> {
+    return this.http.post<LoginResponseToken>(`/api/auth/login`, user)
+      .pipe(
+        tap(({token}) => {
+          localStorage.setItem('auth-token', token);
+          this.setToken(token)
+        })
+      )
+  }
+
+  logout() {
+    this.setToken(null);
+    localStorage.clear();
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.token
+  }
+
+  register() {}
+}
